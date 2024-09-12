4da7ce7769b5d09bb9c2df3a601f52ac79eb0da3
bababoi-2 <165707934+bababoi-2@users.noreply.github.com>
2024-09-12 15:34:16 +0200

    Update reddit_age_bypass_userscript.js

--- reddit_age_bypass_userscript.js
+++ reddit_age_bypass_userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit Age Bypass
 // @namespace    http://tampermonkey.net/
-// @version      1.01
+// @version      1.1
 // @description  Bypass the "open in app prompt" for nsfw posts
 // @author       Bababoiiiii
 // @license      MIT
@@ -13,25 +13,43 @@
 // www.redditstatic.com/shreddit*xpromo-nsfw-blocking-modal-*.js$script,domain=www.reddit.com
 // This Ublock filter can be used as a replacement for this script, i dont know if it has any other impact though
 // www.reddit.com##div.prompt
+
+function log(text) {
+    console.log("[Reddit Age Bypass] " + text);
+}
+
 (function() {
+    "use strict";
+
     function wait_and_remove() {
-        let icon;
-        const wait = setInterval(() => {
+        clearInterval(wait_for_post);
+        wait_for_post = setInterval(() => {
+            log("Waiting for post");
 
-            icon = document.querySelector("span.flex.gap-xs.items-center.pr-xs.truncate > span > faceplate-tracker > a > div");
+            const icon = document.querySelector("span.flex.gap-xs.items-center.pr-xs.truncate > span > faceplate-tracker > a > div");
             if (icon !== null) {
+                clearInterval(wait_for_post);
+                log("Post found");
 
-                clearInterval(wait);
                 // check if the icon is nsfw -> post is nsfw
                 if (icon.querySelector("icon-nsfw") !== null) {
-
-                    let blur_elem;
-                    document.querySelectorAll("xpromo-nsfw-blocking-container").forEach(blur_elem => blur_elem.shadowRoot.querySelector("div.prompt").remove());
-                    console.log("[Reddit Age Bypass] Bypass successfull");
+                    const blurs = document.querySelectorAll("xpromo-nsfw-blocking-container");
+                    log(`Post is NSFW, removing ${blurs.length} blurs`);
+                    blurs.forEach(blur_elem => blur_elem.shadowRoot.querySelector("div.prompt").remove());
+                    log("Bypass successfull");
                 }
             }
-        }, 1000);
+        }, 500);
     }
-    window.navigation.addEventListener("navigate", () => wait_and_remove());
+
+    let wait_for_post;
     wait_and_remove();
+
+    const wait_for_polyfill = setInterval( () => {
+      if (window.navigation) {
+        clearInterval(wait_for_polyfill);
+        window.navigation.addEventListener("navigate", () => wait_and_remove());
+      }
+    }, 10)
+
 })();
